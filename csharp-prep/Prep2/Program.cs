98bcbb27fe563e0bdb01acf2daa925557dfeb0ee
Bezerker907 <meastham907@gmail.com>
2024-09-20 16:18:14 -0600

    Completed the Prep 2 assignment. Updated to make the the console more sassy and condescending.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userGrade = Console.ReadLine();
+        int gradeNumber = int.Parse(userGrade);
+
+        string gradeLetter = "";
+
+        if (gradeNumber >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        string passfail = "";
+        
+        if (gradeNumber >= 70)
+        {
+            passfail = "pass";
+        }
+        else
+        {
+            passfail = "fail";
+        }
+
+
+        Console.WriteLine($"Your lettergrade is {gradeLetter}. That means you have {passfail}ed the course.");
+        if (passfail == "pass")
+        {
+            Console.Write("Good job! Keep up the good work!");
+        }
+        else if (passfail == "fail")
+        {
+            Console.Write("You should really try better next time.");
+        }
+
+
     }
 }
