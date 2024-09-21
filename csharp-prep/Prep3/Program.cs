834abc259f91cc4db3194ea9f7a49aa189f0191e
Bezerker907 <meastham907@gmail.com>
2024-09-21 12:38:38 -0600

    Completed Prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int secret = randomGenerator.Next(1, 100);
+
+        bool win = false;
+        
+        while (win == false)
+        {
+            Console.Write("What is your guess? ");
+            string y = Console.ReadLine();
+            int guess = int.Parse(y);
+        
+                if (guess > secret)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < secret)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("That's correct!");
+                    win = true;
+                }
+        }
+            
+
+
+
+
+
     }
 }
